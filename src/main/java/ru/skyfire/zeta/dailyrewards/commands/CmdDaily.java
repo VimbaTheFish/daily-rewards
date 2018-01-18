9a22a6357b7b28dfafee92c84df0edd4a3e49e84
Ruslan <prostorusa@mail.ru>
2018-01-18 11:00:09 +0300

    Added /dr help command

--- src/main/java/ru/skyfire/zeta/dailyrewards/commands/CmdDaily.java
+++ src/main/java/ru/skyfire/zeta/dailyrewards/commands/CmdDaily.java
@@ -4,10 +4,24 @@ import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 
 public class CmdDaily implements CommandExecutor{
     @Override
     public CommandResult execute(CommandSource sender, CommandContext args) {
+        Text.Builder t = Text.builder();
+        t.append(Text.of(TextColors.AQUA, Text.of("MUSHROOM DANCE,")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("MUSHROOM DANCE,")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("WHAT EVER COULD IT MEAN?")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("IT MEANS USE /dr help FOR SOME HELP!")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.GOLD, Text.of("DailyRewards")));
+        sender.sendMessage(t.build());
         return CommandResult.success();
     }
 }
--- src/main/java/ru/skyfire/zeta/dailyrewards/commands/CmdDailyHelp.java
+++ src/main/java/ru/skyfire/zeta/dailyrewards/commands/CmdDailyHelp.java
@@ -4,10 +4,47 @@ import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 
 public class CmdDailyHelp implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource sender, CommandContext args) {
+        if (!sender.hasPermission("dailyrewards.admin") && sender instanceof Player) {
+            Text.Builder t = Text.builder();
+            t.append(Text.of(TextColors.AQUA, Text.of("Aloha, bro! That's DailyReward info page!")));
+            t.append(Text.NEW_LINE);
+            t.append(Text.of(TextColors.AQUA, Text.of("That's commands you need:")));
+            t.append(Text.NEW_LINE);
+            t.append(Text.of(TextColors.AQUA, Text.of("/dr take - take your reward! Executes on GUI button")));
+            t.append(Text.NEW_LINE);
+            t.append(Text.of(TextColors.AQUA, Text.of("/dr show - shows reward GUI again!")));
+            t.append(Text.NEW_LINE);
+            t.append(Text.of(TextColors.AQUA, Text.of("That's all! Good luck!")));
+            sender.sendMessage(t.build());
+            return CommandResult.success();
+        }
+        Text.Builder t = Text.builder();
+        t.append(Text.of(TextColors.AQUA, Text.of("Aloha, bro! That's DailyReward info page!")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("You are admin, as I see. Cool.")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("/dr take - take your reward! Executes on GUI button")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("/dr show - shows reward GUI again!")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("/dr info <player> - shows info about player")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("/dr set day <day> <player> - set typed day to player")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("/dr set status 1/0 <player> - " +
+                "sets status to player (1 - reward is taken, 0 - reward is not taken)")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("/dr reload - summons kitties")));
+        t.append(Text.NEW_LINE);
+        t.append(Text.of(TextColors.AQUA, Text.of("That's all! Good luck!")));
+        sender.sendMessage(t.build());
         return CommandResult.success();
     }
 }
