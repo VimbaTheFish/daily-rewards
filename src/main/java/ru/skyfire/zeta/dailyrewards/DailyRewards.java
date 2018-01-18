976de84d01b7f16a2c675f1cdaee8fcaf70c66be
Ruslan <prostorusa@mail.ru>
2018-01-18 11:18:13 +0300

    Some dirty stuff

--- src/main/java/ru/skyfire/zeta/dailyrewards/DailyRewards.java
+++ src/main/java/ru/skyfire/zeta/dailyrewards/DailyRewards.java
@@ -21,7 +21,7 @@ import ru.skyfire.zeta.dailyrewards.serializers.RewardDeserializer;
 import java.io.IOException;
 import java.nio.file.Path;
 
-@Plugin(id = "dailyrewards", name = "DailyRewards", version = "1.1.3")
+@Plugin(id = "dailyrewards", name = "DailyRewards", description = "Plugin for rewarding players for every day logging it!")
 public class DailyRewards {
     private ConfigurationNode rootDefNode;
     private ConfigurationNode rootTranslationNode;
